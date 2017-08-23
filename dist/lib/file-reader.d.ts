3e8599688b2adc64c251a333d1d4097dddacdefe
Vikas Gautam <vikasgautam43@gmail.com>
2017-08-23 11:30:07 +0530

    del'd file-reader from dist dir

--- dist/lib/file-reader.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="../../src/index.d.ts" />
-/// <reference types="node" />
-export declare function FileReader(fileOrStdinOrFd: string | (typeof process.stdin) | number, startAt?: number, chunkSizeInBytes?: number): IterableIterator<Buffer>;
